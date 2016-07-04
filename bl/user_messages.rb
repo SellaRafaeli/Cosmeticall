7fc703f532319c394d715255bf13dbda61431921
Lily Mateveyeva <lily.matveyeva@gmail.com>
2016-07-04 14:54:52 -0400

    minor change

--- bl/user_messages.rb
+++ bl/user_messages.rb
@@ -1,9 +1,9 @@
-$user_requests = $mongo.collection('user_requests')
+$user_messages = $mongo.collection('user_messages')
 
 post '/contact_supplier_ajax' do
   	phone_number = params['code']+params['phone_without_code']
   	phone = phone_number.gsub(/\s+/, "").gsub(/-/, "")
-  	user_request = $user_requests.add({phone: phone,
+  	user_message = $user_messages.add({phone: phone,
   					description: params[:description]})
-  	{user_request:user_request}
+  	{user_message:user_message}
 end
