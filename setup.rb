c706c6e488ae1b2ad071d8d19f918a953826983c
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-07-13 00:54:01 -0400

    auto-message.

--- setup.rb
+++ setup.rb
@@ -16,21 +16,21 @@ def get_fullpath
   $root_url + request.fullpath
 end
 
-configure :development, :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///testdb')
+# configure :development, :production do
+#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///testdb')
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+#  ActiveRecord::Base.establish_connection(
+#    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#    :host     => db.host,
+#    :username => db.user,
+#    :password => db.password,
+#    :database => db.path[1..-1],
+#    :encoding => 'utf8'
+#  )
+# end
 
-class Post < ActiveRecord::Base
-end
+# class Post < ActiveRecord::Base
+# end
 
-class Author < ActiveRecord::Base
-end
+# class Author < ActiveRecord::Base
+# end
