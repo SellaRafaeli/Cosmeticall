248b9e2a7b23530afcbc7b7ac8498ce2739b4528
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-09-29 13:53:30 +0300

    auto-message.

--- mw/middleware_incoming.rb
+++ mw/middleware_incoming.rb
@@ -12,7 +12,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['admin_username'], ENV['admin_password']]
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']]
   end
 end
 
