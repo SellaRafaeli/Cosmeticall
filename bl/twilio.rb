8f34ff73dd44788c658388dc304a916a07e9bc06
Sella Rafaeli <sella.rafaeli@gmail.com>
2017-01-11 12:15:12 +0200

    auto-message.

--- bl/twilio.rb
+++ bl/twilio.rb
@@ -13,7 +13,7 @@ account_sid = 'ACfbbb449751b5c36f48fb0aa1f2065774'
 auth_token = ENV['TWILIO_AUTH_TOKEN']
  
 # set up a client to talk to the Twilio REST API 
-# TWILIO_CLIENT ||= Twilio::REST::Client.new account_sid, auth_token
+TWILIO_CLIENT ||= Twilio::REST::Client.new account_sid, auth_token
 
 
 def send_sms(to, body = nil, type, sender_phone)
